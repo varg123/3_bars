975c2d110dcf4575d561196144f80fb75de9cf14
Andrey Skilev <andreyskilev@mail.ru>
2019-03-25 18:28:18 +0500

    Сделан везде один тип кавычек

--- bars.py
+++ bars.py
@@ -4,19 +4,19 @@ import sys
 
 
 def print_bar_info(bar):
-    bar_atributes = bar["properties"]['Attributes']
+    bar_atributes = bar['properties']['Attributes']
     bar_name = bar_atributes['Name']
     address = bar_atributes['Address']
     adm_area = bar_atributes['AdmArea']
     district = bar_atributes['District']
     phone_number = bar_atributes['PublicPhone'][0]['PublicPhone']
-    seats_count = bar["properties"]['Attributes']['SeatsCount']
-    templ_bar_info = """
+    seats_count = bar['properties']['Attributes']['SeatsCount']
+    templ_bar_info = '''
         Название бара: {}
         Адрес: {}, {}, {}
         Количество мест: {}
         Телефон: {}
-    """
+    '''
     bar_info = bar_name, adm_area, district, address, seats_count, phone_number
     print(templ_bar_info.format(*bar_info))
 
@@ -33,20 +33,20 @@ def distance_from(user_coords):
 
 
 def get_seats_count(bar):
-    return bar["properties"]['Attributes']['SeatsCount']
+    return bar['properties']['Attributes']['SeatsCount']
 
 
 def main():
-    with open(sys.argv[1], "rt", encoding="utf8") as data_file:
+    with open(sys.argv[1], 'rt', encoding='utf8') as data_file:
         data_bars = json.loads(data_file.read())
     print('Самый большой бар:')
-    print_bar_info(max(data_bars["features"], key=get_seats_count))
+    print_bar_info(max(data_bars['features'], key=get_seats_count))
     print('Cамый маленький бар: ')
-    print_bar_info(min(data_bars["features"], key=get_seats_count))
-    print("Введите свои координаты через пробел:")
+    print_bar_info(min(data_bars['features'], key=get_seats_count))
+    print('Введите свои координаты через пробел:')
     user_coords = [int(coord) for coord in input().split()]
     print('Самый близкий бар: ')
-    print_bar_info(min(data_bars["features"], key=distance_from(user_coords)))
+    print_bar_info(min(data_bars['features'], key=distance_from(user_coords)))
 
 if __name__ == '__main__':
     main()
